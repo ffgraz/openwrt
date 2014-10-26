493a15082fd6ee7b9b91390149c462b41988fcc1
Alexander List <alex@debian.org>
2014-10-26 11:59:42 +0000

    rebase on TL-WA7510Nv1 support by Stefan Helmert

--- target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wa7210n.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wa7210n.c
@@ -5,124 +5,66 @@
  *  Copyright (C) 2011-2012 Gabor Juhos <juhosg@openwrt.org>
  *  Copyright (C) 2014 Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
  *  Copyright (C) 2014 Alexander List <alex@graz.funkfeuer.at>
+ *  rebased on TL-WA7510Nv1 support,
+ *    Copyright (C) 2012 Stefan Helmert <helst_listen@aol.de>
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
  *  by the Free Software Foundation.
  */
 
-#include <linux/gpio.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/partitions.h>
+#include <linux/platform_device.h>
 
-#include <asm/mach-ath79/ath79.h>
 #include <asm/mach-ath79/ar71xx_regs.h>
+#include <asm/mach-ath79/ath79.h>
 
+#include "dev-dsa.h"
 #include "dev-eth.h"
 #include "dev-gpio-buttons.h"
 #include "dev-leds-gpio.h"
 #include "dev-m25p80.h"
-#include "dev-usb.h"
-#include "dev-wmac.h"
 #include "machtypes.h"
+#include "pci.h"
 
-#define TL_WA7210N_GPIO_LED_3G		27
-#define TL_WA7210N_GPIO_LED_WLAN	 0
-#define TL_WA7210N_GPIO_LED_LAN		17
-#define TL_WA7210N_GPIO_LED_WPS		26
+#include "common.h"
 
-#define TL_WA7210N_GPIO_BTN_WPS		11
-#define TL_WA7210N_GPIO_BTN_SW1		18
-#define TL_WA7210N_GPIO_BTN_SW2		20
 
-#define TL_WA7210N_GPIO_USB_POWER	 8
-
-#define TL_WA7210N_KEYS_POLL_INTERVAL	20	/* msecs */
-#define TL_WA7210N_KEYS_DEBOUNCE_INTERVAL	(3 * TL_WA7210N_KEYS_POLL_INTERVAL)
-
-static const char *tl_wa7210n_part_probes[] = {
-	"tp-link",
-	NULL,
+static const char *tl_wa7210n_v2_part_probes[] = {
+        "tp-link",
+        NULL,
 };
 
-static struct flash_platform_data tl_wa7210n_flash_data = {
-	.part_probes	= tl_wa7210n_part_probes,
+static struct flash_platform_data tl_wa7210n_v2_flash_data = {
+        .part_probes    = tl_wa7210n_v2_part_probes,
 };
 
-static struct gpio_led tl_wa7210n_leds_gpio[] __initdata = {
-	{
-		.name		= "tp-link:green:3g",
-		.gpio		= TL_WA7210N_GPIO_LED_3G,
-		.active_low	= 1,
-	},
-	{
-		.name		= "tp-link:green:wlan",
-		.gpio		= TL_WA7210N_GPIO_LED_WLAN,
-		.active_low	= 0,
-	},
-	{
-		.name		= "tp-link:green:lan",
-		.gpio		= TL_WA7210N_GPIO_LED_LAN,
-		.active_low	= 1,
-	},
-	{
-		.name		= "tp-link:green:wps",
-		.gpio		= TL_WA7210N_GPIO_LED_WPS,
-		.active_low	= 1,
-	},
-};
-
-static struct gpio_keys_button tl_wa7210n_gpio_keys[] __initdata = {
-	{
-		.desc		= "wps",
-		.type		= EV_KEY,
-		.code		= KEY_WPS_BUTTON,
-		.debounce_interval = TL_WA7210N_KEYS_DEBOUNCE_INTERVAL,
-		.gpio		= TL_WA7210N_GPIO_BTN_WPS,
-		.active_low	= 0,
-	},
-	{
-		.desc		= "sw1",
-		.type		= EV_KEY,
-		.code		= BTN_0,
-		.debounce_interval = TL_WA7210N_KEYS_DEBOUNCE_INTERVAL,
-		.gpio		= TL_WA7210N_GPIO_BTN_SW1,
-		.active_low	= 0,
-	},
-	{
-		.desc		= "sw2",
-		.type		= EV_KEY,
-		.code		= BTN_1,
-		.debounce_interval = TL_WA7210N_KEYS_DEBOUNCE_INTERVAL,
-		.gpio		= TL_WA7210N_GPIO_BTN_SW2,
-		.active_low	= 0,
-	}
-};
-
-static void __init tl_wa7210n_setup(void)
+static void __init tl_wa7210n_v2_setup(void)
 {
 	u8 *mac = (u8 *) KSEG1ADDR(0x1f01fc00);
-	u8 *ee = (u8 *) KSEG1ADDR(0x1fff1000);
 
-	/* disable PHY_SWAP and PHY_ADDR_SWAP bits */
-	ath79_setup_ar933x_phy4_switch(false, false);
 
-	ath79_register_m25p80(&tl_wa7210n_flash_data);
-	ath79_register_leds_gpio(-1, ARRAY_SIZE(tl_wa7210n_leds_gpio),
-				 tl_wa7210n_leds_gpio);
-	ath79_register_gpio_keys_polled(-1, TL_WA7210N_KEYS_POLL_INTERVAL,
-					ARRAY_SIZE(tl_wa7210n_gpio_keys),
-					tl_wa7210n_gpio_keys);
+	/* use this to switch off LAN-LED */
+/*        ath79_gpio_function_disable(AR724X_GPIO_FUNC_ETH_SWITCH_LED0_EN |
+                                    AR724X_GPIO_FUNC_ETH_SWITCH_LED1_EN |
+                                    AR724X_GPIO_FUNC_ETH_SWITCH_LED2_EN |
+                                    AR724X_GPIO_FUNC_ETH_SWITCH_LED3_EN |
+                                    AR724X_GPIO_FUNC_ETH_SWITCH_LED4_EN);
+*/
+        /*
+         * ath79_eth0 would be the WAN port, but is not connected on
+         * the TL-WA7510N like the TL-WA901ND. ath79_eth1 connects to the internal switch chip,
+         * however we have a single LAN port only.
+         */
+        ath79_init_mac(ath79_eth1_data.mac_addr, mac, 0);
+        ath79_register_mdio(0, 0x0);
+        ath79_register_eth(1);
 
-	gpio_request_one(TL_WA7210N_GPIO_USB_POWER,
-			 GPIOF_OUT_INIT_HIGH | GPIOF_EXPORT_DIR_FIXED,
-			 "USB power");
-	ath79_register_usb();
+	ath79_register_m25p80(&tl_wa7210n_v2_flash_data);
 
-	ath79_init_mac(ath79_eth0_data.mac_addr, mac, 0);
-
-	ath79_register_mdio(0, 0x0);
-	ath79_register_eth(0);
-	ath79_register_wmac(ee, mac);
+	ath79_register_pci();
 }
 
-MIPS_MACHINE(ATH79_MACH_TL_WA7210N, "TL-WA7210N", "TP-LINK TL-WA7210N",
-	     tl_wa7210n_setup);
+MIPS_MACHINE(ATH79_MACH_TL_WA7210N_V2, "TL-WA7210N", "TP-LINK TL-WA7210N v2",
+	     tl_wa7210n_v2_setup);
